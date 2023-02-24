5aef227d1f09de9c4d215716cb9392cbe4efba1d
scisneros <sebastian.cisneros384@gmail.com>
2023-02-24 17:51:16 -0300

    Initial "Next hours" data show

---
+++ src/components/NextHoursForecast.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    props: {
+        forecastData: Array
+    }
+}
+</script>
+
+<template>
+    <v-card>
+        <v-card-title>Next hours</v-card-title>
+        <template v-for="item in forecastData">
+            <div style="margin-bottom: 16px">
+                <p>{{ item.main.temp }}</p>
+                <p>{{ item.main.humidity }}</p>
+                <p>{{ item.weather[0].main }}</p>
+                <p>{{ item.dt }}</p>
+            </div>
+        </template>
+    </v-card>
+</template>
--- src/views/ForecastView.vue
+++ src/views/ForecastView.vue
@@ -1,15 +1,24 @@
 <script>
 import cities from '../data/cities.js'
+import exampleResponse from '../data/exampleResponse.js'
+
+import NextHoursForecast from '../components/NextHoursForecast.vue'
 
 export default {
+    components: {
+        NextHoursForecast
+    },
     data() {
         return {
             cities: cities,
+            exampleResponse: exampleResponse
         }
     }
 }
 </script>
 
 <template>
-    {{ cities[$route.params.nameId] }}
+    <div class="forecast-container">
+        <NextHoursForecast :forecastData="exampleResponse.list" />
+    </div>
 </template>
