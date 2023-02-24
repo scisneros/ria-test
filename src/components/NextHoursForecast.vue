b3d2b9619965aa02491e837fa21f69e70f4369c0
scisneros <sebastian.cisneros384@gmail.com>
2023-02-24 18:32:28 -0300

    Format times

--- src/components/NextHoursForecast.vue
+++ src/components/NextHoursForecast.vue
@@ -1,13 +1,20 @@
 <script>
+import dayjs from "dayjs"
+
 export default {
     props: {
         forecastData: Array
+    },
+    methods: {
+        formatTime(dt) {
+            return dayjs.unix(dt).format("hh:mm A")
+        }
     }
 }
 </script>
 
 <template>
-    <v-card>
+    <v-card class="forecast-card">
         <v-card-title class="forecast-card-title">Next hours</v-card-title>
         <div class="hourly-container">
             <template v-for="item in forecastData">
@@ -15,7 +22,7 @@ export default {
                     <p class="hourly-temp">{{ Math.round(item.main.temp) }}°</p>
                     <p class="hourly-humid">{{ item.main.humidity }}%</p>
                     <img class="hourly-icon" :src="`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`" />
-                    <p class="hourly-time">{{ item.dt }}</p>
+                    <p class="hourly-time">{{ formatTime(item.dt) }}</p>
                 </div>
                 <div class="hourly-divider"></div>
             </template>
@@ -55,6 +62,7 @@ export default {
 
 .hourly-icon {
     width: 5rem;
+    height: 5rem;
     margin: -4px;
 }
 
