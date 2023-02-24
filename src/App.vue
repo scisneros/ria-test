c3c0db8721dc4d27804e3d78052d779338e8b469
scisneros <sebastian.cisneros384@gmail.com>
2023-02-24 16:28:19 -0300

    Main navbar and city tabs

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,44 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
+
+export default {
+  components: {
+    RouterView,
+    RouterLink
+  },
+  data() {
+    return {
+      selectedTab: null,
+      tabItems: [
+        'Rio de Janeiro', 'Beijing', 'Los Angeles'
+      ]
+    }
+  }
+}
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Nav 1</RouterLink>
-        <RouterLink to="/">Nav 2</RouterLink>
-      </nav>
+      <v-layout>
+        <v-app-bar title="Simple Weather" color="primary" :elevation="2">
+          <template v-slot:append>
+            <v-btn icon="mdi-magnify"></v-btn>
+          </template>
+        </v-app-bar>
+        <v-main>
+          <v-tabs v-model="selectedTab">
+            <v-tab v-for="item in tabItems" :key="item" :value="item">
+              {{ item }}
+            </v-tab>
+          </v-tabs>
+        </v-main>
+      </v-layout>
     </div>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
