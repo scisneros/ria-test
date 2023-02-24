4f4b03375038277c6c574cbe17e797dadf9359fc
scisneros <sebastian.cisneros384@gmail.com>
2023-02-24 19:13:40 -0300

    Daily forecast card (fixed data)

---
+++ src/components/NextDaysForecast.vue
@@ -0,0 +1,85 @@
+<script>
+import dayjs from 'dayjs';
+
+export default {
+    props: {
+        forecastData: Array
+    },
+    methods: {
+        formatTime(dt) {
+            return dayjs.unix(dt).format("ddd, MMM D")
+        }
+    }
+}
+</script>
+
+<template>
+    <v-card class="forecast-card">
+        <v-card-title class="forecast-card-title">Next 5 days</v-card-title>
+        <div class="daily-container">
+            <template v-for="item in forecastData">
+                <div class="daily-item">
+                    <img class="daily-icon" :src="`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`" />
+                    <div class="daily-info">
+                        <p class="daily-date">{{ formatTime(item.dt) }}</p>
+                        <p class="daily-desc">{{ item.weather[0].description }}</p>
+                    </div>
+                    <div class="daily-temp">
+                        <span class="daily-max">{{ Math.round(item.temp.max - 273.15) }}°</span>
+                        <span class="daily-min">{{ Math.round(item.temp.min - 273.15) }}°</span>
+                    </div>
+                </div>
+            </template>
+        </div>
+    </v-card>
+</template>
+
+<style scoped>
+.daily-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: auto;
+}
+
+.daily-item {
+    display: flex;
+    width: 100%;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 8px;
+    border-bottom: 1px solid #EEE;
+}
+
+.daily-info {
+    text-align: center;
+}
+
+.daily-info .daily-date {
+    font-weight: bold;
+}
+
+.daily-info .daily-desc {
+    color: #757575;
+}
+
+.daily-info .daily-desc:first-letter {
+    text-transform: uppercase;
+}
+
+.daily-temp {
+    font-weight: bold;
+    padding-right: 8px;
+}
+
+.daily-temp .daily-max {
+    margin-right: 8px;
+}
+
+.daily-icon {
+    width: 5rem;
+    height: 5rem;
+    margin: -4px;
+}
+</style>
--- src/views/ForecastView.vue
+++ src/views/ForecastView.vue
@@ -1,17 +1,21 @@
 <script>
 import cities from '../data/cities.js'
 import exampleResponse from '../data/exampleResponse.js'
+import exampleResponseDaily from '../data/exampleResponseDaily.js'
 
 import NextHoursForecast from '../components/NextHoursForecast.vue'
+import NextDaysForecast from '../components/NextDaysForecast.vue'
 
 export default {
     components: {
-        NextHoursForecast
+        NextHoursForecast,
+        NextDaysForecast
     },
     data() {
         return {
             cities: cities,
-            exampleResponse: exampleResponse
+            exampleResponse: exampleResponse,
+            exampleResponseDaily: exampleResponseDaily,
         }
     }
 }
@@ -20,6 +24,7 @@ export default {
 <template>
     <div class="forecast-container">
         <NextHoursForecast :forecastData="exampleResponse.list" />
+        <NextDaysForecast :forecastData="exampleResponseDaily.list" />
     </div>
 </template>
 
@@ -28,10 +33,14 @@ export default {
     padding: 16px;
     background: linear-gradient(0deg, #dacec2 19%, #06a9c4 20%, #005c8d 100%); /* Could be an image */
     height: calc(100vh - 64px - 48px); /* Hardcoded navbar and tab height */
+    overflow: auto;
 }
 </style>
 
 <style>
+.forecast-card {
+    margin-bottom: 16px;
+}
 .forecast-card-title {
     border-bottom: 1px solid #EEE;
 }
